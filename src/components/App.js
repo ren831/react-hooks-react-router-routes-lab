a22df2b68bbf5619a94ec85635cf36d9615bc182
ren831 <rene83120@gmail.com>
2022-04-18 12:32:15 -0700

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,23 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Route path="/actors">
+        <Actors />
+      </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/directors">
+        <Directors />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
+import { getByTitle } from "@testing-library/react";
 import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.title}>
+            <h3>Title: {movie.title}</h3>
+            <p> Length: {movie.time}</p>
+            <ul>
+              {movie.genres.map((genre) => {
+                return <li key={genre}>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
